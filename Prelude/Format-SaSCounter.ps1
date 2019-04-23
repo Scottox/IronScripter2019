d1c2b9bfae192495ff13fe15904da1e0488bc337
Scott Stevenson <sstevenson@terrapower.com>
2019-04-23 14:38:04 -0700

    initial commit

---
+++ Prelude/Format-SaSCounter.ps1
@@ -0,0 +1,51 @@
+<#
+Your challenge is to create a PowerShell function that takes the output from Get-Counter and transforms it into a more user friendly object. Each counter sample should be a separate object with these properties:
+Timestamp
+Computername
+CounterSet
+Counter
+Value
+#>
+
+<#
+.SYNOPSIS
+    Formats output from Get-Counter
+.DESCRIPTION
+    Formats output from Get-Counter cmdlet to improve readability
+.EXAMPLE
+    Get-Counter | Format-SaSCounter
+#>
+function Format-SaSCounter {
+    [CmdletBinding(PositionalBinding=$true)]
+    Param (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   Position=0,
+                   ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true)]
+        [ValidateNotNullOrEmpty()]
+        $CounterSet
+
+    )
+
+    process {
+        $result = @()
+        foreach ( $counter in $CounterSet) {
+            foreach ( $c in $counter.countersamples ) {
+                $cArray = @()
+                $cArray = $c.path.split('\')
+                $result += [PSCustomObject]@{
+                    Timestamp = $c.Timestamp
+                    Computername = $cArray[2]
+                    CounterSet = $cArray[3]
+                    Counter = $cArray[4]
+                    Value = $c.CookedValue
+                }
+            }
+        }
+    }
+    
+    end {
+        return $result
+    }
+}
---
+++ Prequel1.ps1
@@ -0,0 +1,7 @@
+<#
+The Chairman has arranged for another prequel challenge to prepare you for the upcoming Iron Scripter battle. A suggested solution will eventually be provided. In the mean time, see what you can do with this problem.
+
+Using a set of Windows features, like Windows Internal Database, on a baseline server, configure a target server to match. Remove features that aren’t installed on the baseline and install features that are missing on the target. You can configure the source server with any combination of Windows features you’d like. Your goal is to copy the feature set to a target server.
+
+This does not need to be a one-liner or even a function, although you are welcome to do so. What commands would you run in a PowerShell remoting session to accomplish this task? While it is true you could use a technology like Desired State Configuration, see if you can accomplish this task through traditional PowerShell scripting techniques.
+#>
